2c90dcd2323b7c05d2d657a99e2bb115516dbbc0
Alordome Caleb Kofi <61848301+Alordome@users.noreply.github.com>
2023-08-01 02:47:50 +0000

    Update sign_in.dart

--- lib/screens/signIn/sign_in.dart
+++ lib/screens/signIn/sign_in.dart
@@ -1,24 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:trashout/components/loginButton.dart';
+import 'package:trashout/screens/signIn/sign_in_as.dart';
+import 'package:trashout/services/auth_service.dart';
 
 import '../../components/loginInputField.dart';
 import '../../components/squaretile.dart';
 import '../home/home.dart';
 
-class SignIn extends StatefulWidget {
-  const SignIn({super.key});
+class SignIn extends StatelessWidget {
+  SignIn({Key? key}) : super(key: key);
 
-
-
-  @override
-  State<SignIn> createState() => _SignInState();
-}
-
-class _SignInState extends State<SignIn> {
-
-  // text editing controllers
-  final emailController = TextEditingController();
-  final passwordController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -28,54 +21,84 @@ class _SignInState extends State<SignIn> {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              const SizedBox(height: 70,),
+              const SizedBox(
+                height: 70,
+              ),
               Container(
-                  width: 100,
-                  height: 100,
-                  decoration:  const BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Color(0xff00bf63),
+                width: 100,
+                height: 100,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Color(0xff00bf63),
+                ),
+                child: const Center(
+                  child: Icon(
+                    Icons.person,
+                    size: 90,
+                    color: Colors.white,
                   ),
-                  child:const Center(child:  Icon(Icons.person, size: 90, color: Colors.white,))),
-              const SizedBox(height: 40,),
-              // Text("Welcome Back User",
-              //   style: TextStyle(color: Colors.grey[700]),
-              // ),
-              const SizedBox(height: 20,),
+                ),
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
               LoginInputField(
                 controller: emailController,
                 hintText: 'Email',
                 obscureText: false,
+                validator: (value) {
+                  return null;
+                },
               ),
-              const SizedBox(height: 20.0,),
+              const SizedBox(
+                height: 20.0,
+              ),
               LoginInputField(
                 controller: passwordController,
                 hintText: 'Password',
                 obscureText: true,
+                validator: (value) {
+                  return null;
+                },
               ),
-              const SizedBox(height: 20,),
+              const SizedBox(
+                height: 20,
+              ),
               const Padding(
-                padding: EdgeInsets.fromLTRB(25, 0 , 25, 0),
-                child:  Row(
+                padding: EdgeInsets.fromLTRB(25, 0, 25, 0),
+                child: Row(
                   mainAxisAlignment: MainAxisAlignment.end,
                   children: [
-                    Text("Forgot Password?",
-                    style: TextStyle(
-                      fontSize: 15,
-                      color: Colors.blue,
-                    )
-                    )
+                    Text(
+                      "Forgot Password?",
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.blue,
+                      ),
+                    ),
                   ],
                 ),
               ),
-              const SizedBox(height:55,),
+              const SizedBox(
+                height: 55,
+              ),
               LoginButton(
-                      onTap: () { Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => const HomePage()
-                          ));
-              }),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const HomePage(),
+                    ),
+                  );
+                },
+              ),
 
-             const  SizedBox(height: 20,),
+              const SizedBox(
+                height: 20,
+              ),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 25.0),
                 child: Row(
@@ -86,13 +109,13 @@ class _SignInState extends State<SignIn> {
                         color: Colors.grey[400],
                       ),
                     ),
-
                     Padding(
                       padding: const EdgeInsets.symmetric(horizontal: 10.0),
                       child: Text(
                         'OR ',
-                        style: TextStyle(color: Colors.grey[700],
-                        fontSize: 18,
+                        style: TextStyle(
+                          color: Colors.grey[700],
+                          fontSize: 18,
                         ),
                       ),
                     ),
@@ -108,40 +131,55 @@ class _SignInState extends State<SignIn> {
               const SizedBox(height: 50),
 
               // google + facebook sign in buttons
-              const Row(
+              Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   // google button
-                  SquareTile(imagePath: 'assets/google_logo.png'),
+                  SquareTile(
+                    onTap: () => AuthService.signInWithGoogle(),
+                    imagePath: 'assets/google_logo.png',
+                  ),
+                  const SizedBox(width: 25),
 
-                  SizedBox(width: 25),
-
-                  // apple button
-                  SquareTile(imagePath: 'assets/facebook_logo.png')
+                  // facebook button
+                  SquareTile(
+                    onTap: () {},
+                    imagePath: 'assets/facebook_logo.png',
+                  ),
                 ],
               ),
 
               const SizedBox(height: 50),
 
               // not a member? register now
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Not a member?',
-                    style: TextStyle(color: Colors.grey[700]),
-                  ),
-                  const SizedBox(width: 4),
-                  const Text(
-                    'Register now',
-                    style: TextStyle(
-                      color: Colors.blue,
-                      fontWeight: FontWeight.bold,
+              GestureDetector(
+                onTap: () {
+                  // Replace 'DesiredDestinationScreen()' with the actual screen you want to navigate to.
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => RegisterScreen(),
                     ),
-                  ),
-                ],
-              )
-
+                  );
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Not a member?',
+                      style: TextStyle(color: Colors.grey[700]),
+                    ),
+                    const SizedBox(width: 4),
+                    const Text(
+                      'Register now',
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
@@ -149,4 +187,3 @@ class _SignInState extends State<SignIn> {
     );
   }
 }
-
