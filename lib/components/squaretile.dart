fe1261537b47de06ed79ab15e4f0c0a813dda515
Alordome Caleb Kofi <61848301+Alordome@users.noreply.github.com>
2023-08-01 02:48:41 +0000

    Update squaretile.dart

--- lib/components/squaretile.dart
+++ lib/components/squaretile.dart
@@ -2,22 +2,29 @@ import 'package:flutter/material.dart';
 
 class SquareTile extends StatelessWidget {
   final String imagePath;
+  final Function()? onTap;
   const SquareTile({
-    super.key,
+    Key? key,
     required this.imagePath,
-  });
+    required this.onTap,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(20),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16),
-        color: Colors.grey[200],
-      ),
-      child: Image.asset(
-        imagePath,
-        height: 40,
+    return GestureDetector(
+      onTap: onTap,
+      child: Padding(
+        padding: const EdgeInsets.all(20), // Wrap padding inside Padding widget
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            color: Colors.grey[200],
+          ),
+          child: Image.asset(
+            imagePath,
+            height: 40,
+          ),
+        ),
       ),
     );
   }
